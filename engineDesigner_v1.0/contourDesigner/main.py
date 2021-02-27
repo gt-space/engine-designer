41be975bb8558dc7b4e9ffb268ff7f9b870290ef
woodmanben <woodmanben01@gmail.com>
2021-02-26 20:27:39 -0500

    new file setup

---
+++ engineDesigner_v1.0/contourDesigner/main.py
@@ -0,0 +1,46 @@
+# USAGE:
+# design_engine(thrust_nom, P_inj, conRat, MR, divAng)
+# thrust_nom: Design thrust (N)
+# P_inj: Injector face pressure (Bar)
+# conRat: Enigne contraciton ratio
+# MR: ox/fuel (default = 1.8)
+# divAng: Divergence half angle (Deg) (defualt = 15)
+
+# OUTPUTS:
+# engineContour is a numpy array with the following column format:
+# [R, Z]
+# engineProps is a numpy array with the following column format:
+# [R, Z, pip, aeat, mach, cf, ivac, isp, p, t, rho, h, u, mw, cp, gam, son,
+# vis, cond, pran, cpfz, condfz, pranfz]
+
+from engine_designer.dataCollectionScript import Engine
+import matplotlib.pyplot as plt
+import numpy as np
+
+thrust = 17792.89 # thrust (N)
+P_c = 18 #48.2633 # chamber pressure (bar)
+conrat = 6
+LStar = 1.2
+
+# Run it!
+engine = Engine(thrust, P_c, conrat, LStar = LStar)
+engine.design_engine()
+print("Exit_vel: " + str(engine.V_exit))
+print("M_dot: " + str(engine.mDot_tot))
+print("Throat: " + str(engine.A_t))
+
+# print(engine.A_t)
+print("C-star:", engine.C_star)
+
+# See a specific value (change the 0 to the index of the property you want to see):
+plt.plot(engine.engineProps[:, 1], engine.engineProps[:,0])
+plt.xlabel('Distance from Injector (m)', fontsize=16)
+plt.ylabel('Radius (m)', fontsize=16)
+# plt.ylim([0,0.1])
+plt.show()
+
+def create_csv(engine):
+    matrix = engine.engineProps[:, 0:2] # Radius and distance from injector matrix
+    np.savetxt("contour.csv", matrix, delimiter=",")
+
+# create_csv(engine)
