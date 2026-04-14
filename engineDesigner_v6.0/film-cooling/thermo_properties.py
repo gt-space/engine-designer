314c2c31b6e8df6a780c1d4a7ecd033f31a2f6b8
pl256231 <pl256231@ahschool.com>
2026-04-14 10:31:11 -0400

    object oriented thermo_prop 2

--- engineDesigner_v6.0/film-cooling/thermo_properties.py
+++ engineDesigner_v6.0/film-cooling/thermo_properties.py
@@ -1,19 +1,19 @@
 from Coolprop.Coolprop import PropsSI
 
-def get_liquid_props(T_L, P_cc):
-    rho_l = PropsSI('D', 'T', T_L, 'P', P_cc, 'dodecane')
-    mu_l = PropsSI('V', 'T', T_L, 'P', P_cc, 'dodecane')
-    Cp_l = PropsSI('Cp0mass', 'T', T_L, 'P', P_cc, 'dodecane')
-    sigma_l = PropsSI('I', 'T', T_L, 'P', P_cc, 'dodecane')
-    H_V = PropsSI('H', 'P', P_cc, 'Q', 1, 'dodecane')
-    H_L = PropsSI('H', 'P', P_cc, 'Q', 0, 'dodecane')
-    LHV = H_V-H_L
-    T_sat = PropsSI('T', 'P', P_cc, 'Q', 0, 'dodecane')
-    return rho_l, mu_l, Cp_l, sigma_l, LHV, T_sat
+class Liquid_Props:
+    def __init__(self, T_L, P_cc):
+        self.rho_l = rho_l = PropsSI('D', 'T', T_L, 'P', P_cc, 'dodecane')
+        self.mu_l = PropsSI('V', 'T', T_L, 'P', P_cc, 'dodecane')
+        self.Cp_l = PropsSI('Cp0mass', 'T', T_L, 'P', P_cc, 'dodecane')
+        self.sigma_l = PropsSI('I', 'T', T_L, 'P', P_cc, 'dodecane')
+        H_V = PropsSI('H', 'P', P_cc, 'Q', 1, 'dodecane')
+        H_L = PropsSI('H', 'P', P_cc, 'Q', 0, 'dodecane')
+        self.LHV = H_V-H_L
+        self.T_sat = PropsSI('T', 'P', P_cc, 'Q', 0, 'dodecane')
 
-def get_gas_props(T_L, P_cc):
-    rho_g = PropsSI('D', 'T', T_L, 'P', P_cc, 'dodecane')
-    mu_g = PropsSI('V', 'T', T_L, 'P', P_cc, 'dodecane')
-    Cp_g = PropsSI('Cp0mass', 'T', T_L, 'P', P_cc, 'dodecane')
-    Pr_g = PropsSI('Prandtl', 'T', T_L, 'P', P_cc, 'dodecane')
-    return rho_g, mu_g, Cp_g, Pr_g
+class Gas_Props:
+    def __init__(self, T_L, P_cc):
+        self.rho_g = PropsSI('D', 'T', T_L, 'P', P_cc, 'dodecane')
+        self.mu_g = PropsSI('V', 'T', T_L, 'P', P_cc, 'dodecane')
+        self.Cp_g = PropsSI('Cp0mass', 'T', T_L, 'P', P_cc, 'dodecane')
+        self.Pr_g = PropsSI('Prandtl', 'T', T_L, 'P', P_cc, 'dodecane')
