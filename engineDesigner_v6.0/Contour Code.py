53d8fe49dd48bd2cfcf559fd4872a0c746d3f20e
TysonNT1226 <ttran409@gatech.edu>
2026-09-21 20:16:39 -0400

    Change engine setpoints

--- engineDesigner_v6.0/Contour Code.py
+++ engineDesigner_v6.0/Contour Code.py
@@ -21,11 +21,11 @@ from rocketisp.geometry import Geometry
 from rao_data import theta_e_data, theta_n_data
 
 # --- 1. PRIMARY ENGINE INPUTS ---
-Pc = 250.0             # psia, Chamber Pressure
-Pe = 7.0               # psia, Exit Pressure (for perfect expansion)
-T_target = 3174.0      # lbf, Target Actual Thrust
-MR = 2.2               # Mixture Ratio
-Cstar_eff = 0.9        # C* Efficiency 
+Pc = 261.63             # psia, Chamber Pressure
+Pe = 11.0               # psia, Exit Pressure 
+T_target = 3597.0      # lbf, Target Actual Thrust
+MR = 2.1               # Mixture Ratio
+Cstar_eff = 0.85        # C* Efficiency 
 Cf_eff = 0.95          # Cf Efficiency 
 
 # Initialize Propellants
